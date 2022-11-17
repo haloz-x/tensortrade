1d3455a4053c3b8114f56c1ecde9b0a494aeed23
haloz <heng0112@qq.com>
2022-11-17 09:29:57 +0800

    ray-test

--- examples/test-ray.py
+++ examples/test-ray.py
@@ -1,93 +1,24 @@
-# import pandas as pd
-#
-# df = pd.read_csv("data/ETH-USDT-SWAP.csv", index_col=0, parse_dates=True)
-#
-#
-# ohlc = {
-#     'open': 'first',
-#     'high': 'max',
-#     'low': 'min',
-#     'close': 'last',
-#     'volume': 'sum'
-# }
-#
-# # df2 = df.resample('15min', base=15).apply(ohlc)
-# #
-# #
-# # print(df2.head(300))
-# x = {k: "LTC:" + k for k in ['open', 'high', 'low', 'close', 'volume']}
-#
-# print(x)
-#
-import matplotlib.pyplot as plt
+import ray
 import numpy as np
-import pandas as pd
-from ray.tune.registry import register_env
 
-import tensortrade.env.default as default
-from tensortrade.env.default.actions import BSH
-from tensortrade.env.default.rewards import PBR
-from tensortrade.env.generic import Renderer
-from tensortrade.feed.core import DataFeed, Stream
-from tensortrade.oms.exchanges import Exchange
-from tensortrade.oms.instruments import USD, Instrument
-from tensortrade.oms.services.execution.simulated import execute_order
-from tensortrade.oms.wallets import Wallet, Portfolio
 from ray import tune
 from ray.tune.registry import register_env
 
+import tensortrade.env.default as default
+
+from tensortrade.feed.core import DataFeed, Stream
+from tensortrade.oms.instruments import Instrument
+from tensortrade.oms.exchanges import Exchange
+from tensortrade.oms.services.execution.simulated import execute_order
+from tensortrade.oms.wallets import Wallet, Portfolio
+
+USD = Instrument("USD", 2, "U.S. Dollar")
 TTC = Instrument("TTC", 8, "TensorTrade Coin")
 
 
-class PositionChangeChart(Renderer):
-
-    def __init__(self, color: str = "orange"):
-        self.color = "orange"
-
-    def render(self, env, **kwargs):
-        history = pd.DataFrame(env.observer.renderer_history)
-
-        actions = list(history.action)
-        p = list(history.price)
-
-        buy = {}
-        sell = {}
-
-        for i in range(len(actions) - 1):
-            a1 = actions[i]
-            a2 = actions[i + 1]
-
-            if a1 != a2:
-                if a1 == 0 and a2 == 1:
-                    buy[i] = p[i]
-                else:
-                    sell[i] = p[i]
-
-        buy = pd.Series(buy)
-        sell = pd.Series(sell)
-
-        fig, axs = plt.subplots(1, 2, figsize=(15, 5))
-
-        fig.suptitle("Performance")
-
-        axs[0].plot(np.arange(len(p)), p, label="price", color=self.color)
-        axs[0].scatter(buy.index, buy.values, marker="^", color="green")
-        axs[0].scatter(sell.index, sell.values, marker="^", color="red")
-        axs[0].set_title("Trading Chart")
-
-        performance_df = pd.DataFrame().from_dict(env.action_scheme.portfolio.performance, orient='index')
-        performance_df.plot(ax=axs[1])
-        axs[1].set_title("Net Worth")
-
-        plt.show()
-
-
 def create_env(config):
-    x = np.arange(0, 2 * np.pi, 2 * np.pi / 1001)
-    y = 50 * np.sin(3 * x) + 100
-
     x = np.arange(0, 2 * np.pi, 2 * np.pi / 1000)
-    p = Stream.source(y, dtype="float").rename("USD-TTC")
+    p = Stream.source(50 * np.sin(3 * x) + 100, dtype="float").rename("USD-TTC")
 
     bitfinex = Exchange("bitfinex", service=execute_order)(
         p
@@ -109,38 +40,30 @@ def create_env(config):
         p.log().diff().fillna(0).rename("lr")
     ])
 
-    reward_scheme = PBR(price=p)
+    reward_scheme = default.rewards.PBR(price=p)
 
-    action_scheme = BSH(
+    action_scheme = default.actions.BSH(
         cash=cash,
         asset=asset
     ).attach(reward_scheme)
 
-    renderer_feed = DataFeed([
-        Stream.source(y, dtype="float").rename("price"),
-        Stream.sensor(action_scheme, lambda s: s.action, dtype="float").rename("action")
-    ])
-
-    environment = default.create(
+    env = default.create(
         feed=feed,
         portfolio=portfolio,
         action_scheme=action_scheme,
         reward_scheme=reward_scheme,
-        renderer_feed=renderer_feed,
-        renderer=PositionChangeChart(),
         window_size=config["window_size"],
         max_allowed_loss=0.6
     )
-    return environment
+    return env
 
 
 register_env("TradingEnv", create_env)
 
-
 analysis = tune.run(
     "PPO",
     stop={
-      "episode_reward_mean": 500
+        "episode_reward_mean": 500
     },
     config={
         "env": "TradingEnv",
@@ -148,7 +71,7 @@ analysis = tune.run(
             "window_size": 25
         },
         "log_level": "DEBUG",
-        "framework": "torch",
+        "framework": "tf2",
         "ignore_worker_failures": True,
         "num_workers": 1,
         "num_gpus": 0,
